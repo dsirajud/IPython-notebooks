19e2d5d9927c2a6941c1f3e44992c4afa9b1fc9b
David Sirajuddin <sirajuddin@wisc.edu>
2016-01-31 17:56:23 -0600

    added several notebooks: Cython-01, Cython-02, Cython-03, which are mostly unfinished but contain some interesting performance demonstrations. DECSKS-22 is finished and redesigns the remap_routine in DECSKS to be a looping mechanism that is coded in Cython, signficant efficiency gains (~ 30 times on a method-by-method comparison) and approximately 3 times savings on a time step by time step comparison with previous implementations.

---
+++ DECSKS-22 -- Cython implementation of remap_assignment/v2.0/DECSKS/lib/boundaryconditions.pyx
@@ -0,0 +1,208 @@
+import numpy as np
+cimport numpy as np
+cimport cython
+
+DTYPE = np.float64
+DTYPEINT = np.int64
+
+ctypedef np.float64_t DTYPE_t
+ctypedef np.int64_t DTYPEINT_t
+
+# PYTHON METHODS
+
+def periodic(f_old,
+             Uf,
+             z,
+             vz,
+             sim_params,
+             charge,
+             k = 0
+             ):
+    """Applies periodic boundary conditions to
+    postpointmesh
+
+    inputs:
+    f_old -- (ndarray, ndim=2) density array
+    z -- (instance) phase space variable being evolved
+
+        z.postpointmesh -- (ndarray, ndim=3),
+                           shape = (2, x.N, vx.N)
+
+    outputs:
+    f_old -- (ndarray, ndim=2) Array with both periodic
+             BCs being enforce
+    z    -- (instance) phase sapce variable being evolved with
+             updated attribute z.postpointmesh
+
+    f_old, Uf returned for symmetry with nonperiodic routine below
+    """
+    z.postpointmesh[k,:,:] = np.mod(z.postpointmesh[k,:,:], z.N)
+
+    return f_old, Uf
+
+def nonperiodic(f_old,
+                Uf,
+                z,
+                vz,
+                sim_params,
+                charge,
+                k = 0
+                ):
+    """orchestrates applying nonperiodic boundary conditions
+    to the array w with total active grid points Nw. Nonperiodic
+    boundary conditions require handling both left and right
+    boundaries
+
+    inputs:
+    f_old -- (ndarray, ndim=2) density array
+    z -- (instance) phase space variable being evolved
+
+    outputs:
+    f_old -- (ndarray, ndim=2) density with both left and right
+             nonperiodic BCs enforced
+    Uf -- (ndarray, ndim=2) high order fluxes with both left and right
+             nonperiodic BCs enforced
+
+
+    z returned (no changes) for symmetry with periodic routine above
+    """
+    # lower boundary
+    f_old, Uf = eval(sim_params['BC'][z.str]['lower'] +
+                           '_lower_boundary')(f_old,
+                                              Uf,
+                                              z.postpointmesh[k,:,:],
+                                              vz.prepointmesh,
+                                              z.N, vz.N, k, charge,
+                                              sim_params,
+                                              z, vz)
+
+    # upper boundary
+    f_old, Uf = eval(sim_params['BC'][z.str]['upper'] +
+                           '_upper_boundary')(f_old,
+                                              Uf,
+                                              z.postpointmesh[k,:,:],
+                                              vz.prepointmesh,
+                                              z.N, vz.N, k, charge,
+                                              sim_params,
+                                              z, vz)
+
+
+    return f_old, Uf
+
+# CYTHON METHODS
+@cython.boundscheck(False)
+def absorbing_lower_boundary(np.ndarray[DTYPE_t, ndim=2] f_old,
+                              np.ndarray[DTYPE_t, ndim=2] Uf_old,
+                              np.ndarray[DTYPEINT_t, ndim=2] zpostpointmesh,
+                              np.ndarray[DTYPEINT_t, ndim=2] vzprepointmesh,
+                              int Nz, int Nvz, int k, int charge,
+                              sim_params,
+                              z, vz):
+
+    # vars here are typed as C data types to minimize python interaction
+    cdef int i, j
+    for i in range(Nz):
+        for j in range(Nvz):
+            if zpostpointmesh[i,j] <= 0:
+                f_old[i,j] = 0
+                Uf_old[i,j] = 0
+                zpostpointmesh[i,j] = 0 # set postpoint at the absorber
+
+    # permanently copy to instance attribute
+    z.postpointmesh[k,:,:] = zpostpointmesh
+
+    return f_old, Uf_old
+
+@cython.boundscheck(False)
+def absorbing_upper_boundary(np.ndarray[DTYPE_t, ndim=2] f_old,
+                              np.ndarray[DTYPE_t, ndim=2] Uf_old,
+                              np.ndarray[DTYPEINT_t, ndim=2] zpostpointmesh,
+                              np.ndarray[DTYPEINT_t, ndim=2] vzprepointmesh,
+                              int Nz, int Nvz, int k, int charge,
+                              sim_params,
+                              z, vz):
+
+    # vars here are typed as C data types to minimize python interaction
+    cdef int i, j
+    for i in range(Nz):
+        for j in range(Nvz):
+            if zpostpointmesh[i,j] >= Nz - 1:
+                f_old[i,j] = 0
+                Uf_old[i,j] = 0
+
+                zpostpointmesh[i,j] = Nz - 1 # set postpoint at the absorber
+
+    # permanently copy to instance attribute
+    z.postpointmesh[k,:,:] = zpostpointmesh
+
+    return f_old, Uf_old
+
+@cython.boundscheck(False)
+def charge_collection_lower_boundary(
+        np.ndarray[DTYPE_t, ndim=2] f_old,
+        np.ndarray[DTYPE_t, ndim=2] Uf_old,
+        np.ndarray[DTYPEINT_t, ndim=2] zpostpointmesh,
+        np.ndarray[DTYPEINT_t, ndim=2] vzprepointmesh,
+        int Nz, int Nvz, int k, int charge,
+        sim_params,
+        z, vz
+        ):
+    # vars here are typed as C data types to minimize python interaction
+
+    # for any such density packet whose postpoint predicts exceeding the lower boundary of
+    # the domain, zero out (absorb) the density, and add contribution to total charge
+    # density sigma at that boundary
+    cdef int i, j
+    cdef DTYPE_t sigma_n = 0 # at current time step n
+    cdef DTYPE_t vzwidth = vz.width
+    for i in range(Nz):
+        for j in range(Nvz):
+            if zpostpointmesh[i,j] <= 0:
+                sigma_n += vzprepointmesh[i,j] * f_old[i,j]
+                f_old[i,j] = 0
+                Uf_old[i,j] = 0
+                zpostpointmesh[i,j] = 0 # set postpoint at the absorber
+
+    sigma_n *= charge
+    sigma_n *= vzwidth
+
+    # update cumulative charge density
+    sim_params['sigma'][z.str]['lower'] += sigma_n
+    z.postpointmesh[k,:,:] = zpostpointmesh
+
+    return f_old, Uf_old
+
+@cython.boundscheck(False)
+def charge_collection_upper_boundary(
+        np.ndarray[DTYPE_t, ndim=2] f_old,
+        np.ndarray[DTYPE_t, ndim=2] Uf_old,
+        np.ndarray[DTYPEINT_t, ndim=2] zpostpointmesh,
+        np.ndarray[DTYPEINT_t, ndim=2] vzprepointmesh,
+        int Nz, int Nvz, int k, int charge,
+        sim_params,
+        z, vz
+        ):
+    # vars here are typed as C data types to minimize python interaction
+
+    # for any such density packet whose postpoint predicts exceeding the lower boundary of
+    # the domain, zero out (absorb) the density, and add contribution to total charge
+    # density sigma at that boundary
+    cdef int i, j
+    cdef DTYPE_t sigma_n = 0 # at current time step n
+    cdef DTYPE_t vzwidth = vz.width
+    for i in range(Nz):
+        for j in range(Nvz):
+            if zpostpointmesh[i,j] >=  Nz - 1:
+                sigma_n += vzprepointmesh[i,j] * f_old[i,j]
+                f_old[i,j] = 0
+                Uf_old[i,j] = 0
+                zpostpointmesh[i,j] = Nz - 1  # set postpoint at the absorber
+
+    sigma_n *= charge
+    sigma_n *= vzwidth
+
+    # update cumulative charge density
+    sim_params['sigma'][z.str]['upper'] += sigma_n
+    z.postpointmesh[k,:,:] = zpostpointmesh
+
+    return f_old, Uf_old
